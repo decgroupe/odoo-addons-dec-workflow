18cd9d66cb1f85b1470bad0d1abe079d47b6cd97
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-02 14:24:56 +0200

    Move picking type data to dedicated stock module

---
+++ stock_workflow_dec/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Stock workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Stock and pickings''',
+    'depends': ['dec', 'stock'],
+    #'force_migration':'12.0.0.0.0',
+    'data': ['data/picking_type.xml', ],
+    'installable': True
+}

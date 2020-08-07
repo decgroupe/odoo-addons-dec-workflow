8dce1808f1abd7689619b5fc352a6a6d791c111a
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-07 21:41:14 +0200

    New modules to create all manufacuring orders at once

--- stock_workflow_dec/__manifest__.py
+++ stock_workflow_dec/__manifest__.py
@@ -4,7 +4,11 @@
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
     'summary': '''Stock and pickings''',
-    'depends': ['dec', 'stock'],
+    'depends': [
+        'dec',
+        'stock',
+        'stock_split_procure_method',
+    ],
     #'force_migration':'12.0.0.0.0',
     'data':
         [

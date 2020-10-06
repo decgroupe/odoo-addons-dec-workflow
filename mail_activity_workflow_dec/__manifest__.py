4c1e6e6921d6dc23fe8ae0d1460709d6282f1cce
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-06 14:50:45 +0200

    Enable "My activities" as default filter

---
+++ mail_activity_workflow_dec/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Mail activity workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''View customization''',
+    'depends': [
+        'mail',
+        'mail_activity_board',
+    ],
+    'data': [
+        'views/mail_activity.xml',
+    ],
+    'installable': True
+}

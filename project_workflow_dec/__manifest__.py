af261df843a86093f2a22426619360f184d6713a
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-18 22:02:39 +0200

    New module for project view dedicted to DEC workflow

---
+++ project_workflow_dec/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Project Workflow Dec',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Custom views for project''',
+    'depends': [
+        'dec',
+        'project_category',
+    ],
+    'data': [
+        'views/project_project.xml',
+        # 'views/project_task.xml',
+    ],
+    'installable': True
+}

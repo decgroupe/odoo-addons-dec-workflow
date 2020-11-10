27d26d495ab59ab4ad3d1b2a0ce5362539ad2a80
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-10 16:30:38 +0100

    New module to store product folder path

---
+++ partner_workflow_dec/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Partner workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com/',
+    'summary': 'Add extra fields on default views',
+    'depends': [
+        'base',
+        'base_location',
+    ],
+    'data': [
+        'views/partner.xml',
+    ],
+    'installable': True
+}
---
+++ partner_workflow_dec/models/res_partner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Nov 2020
+
+from odoo import _, api, models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+

6c50bbd68371e496a1e248ca02f23a2aa951bd7a
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-24 08:35:29 +0100

    Dedicated module for DEC shopping workflow

---
+++ website_sale_workflow_dec/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Website Sale workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': 'Website Sale',
+    'depends': [
+        'website_sale',
+        'website_sale_main_category',
+    ],
+    'data': [],
+    'installable': True
+}

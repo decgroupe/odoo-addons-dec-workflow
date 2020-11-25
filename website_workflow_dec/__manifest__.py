a269c5f57c8e33acb4685d8d28753e4f53333fa9
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-25 14:39:46 +0100

    Allow website editor to edit and save website page bottom changes

---
+++ website_workflow_dec/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Website workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': 'Website',
+    'depends': [
+        'website',
+        'dec',
+    ],
+    'data': [
+        'security/model_security.xml'
+    ],
+    'installable': True
+}

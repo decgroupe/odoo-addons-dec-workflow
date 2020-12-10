584fc7eadf01c507c0826233a3cd8568911e3afb
Yann Papouin <y.papouin@dec-industrie.com>
2020-12-10 14:35:27 +0100

    Allow everyone to change meeting owner

---
+++ calendar_workflow_dec/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Calendar workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''View customization''',
+    'depends': [
+        'calendar',
+    ],
+    'data': [
+        'views/calendar.xml',
+    ],
+    'installable': True
+}

9ba76d86c8a89be0a2037110f30eaf642a807310
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-06 15:43:05 +0100

    Emails are now rendered smaller

---
+++ mail_tracking_workflow/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Email tracking workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Sales',
+    'summary': '''Custom rendering''',
+    'depends': [
+        'mail_tracking',
+    ],
+    'data':
+        [
+            'views/assets.xml',
+        ],
+    'installable': True
+}

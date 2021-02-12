7ff65cd9995292cd8f2fa6c5d776bf505bc11627
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-12 11:36:47 +0100

    Do not filter by task, only by project

---
+++ hr_workflow_dec/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'HR workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Human resources Support''',
+    'depends': [
+        'hr_timesheet',
+    ],
+    'data': [
+        'views/hr_timesheet.xml',
+    ],
+    'installable': True
+}

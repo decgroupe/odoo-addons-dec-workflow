aef89a026e03a5ecffd7bc23fca5caa8bff6d27f
Yann Papouin <y.papouin@dec-industrie.com>
2021-03-25 17:21:45 +0100

    Custom naming to quickly identify a ticket

--- helpdesk_workflow_dec/models/helpdesk_ticket.py
+++ helpdesk_workflow_dec/models/helpdesk_ticket.py
@@ -2,8 +2,11 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
 
+import re
+
 from odoo import _, api, models, fields
 from werkzeug import url_encode
+from odoo.osv import expression
 
 
 class HelpdeskTicket(models.Model):
@@ -18,3 +21,105 @@ class HelpdeskTicket(models.Model):
             vals['project_id'] = project_support_id.id
         ticket_id = super().create(vals)
         return ticket_id
+
+    @api.multi
+    @api.depends('name', 'number')
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = ('[%s] %s') % (rec.number, rec.name)
+            result.append((rec.id, name))
+        return result
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        # Make a search with default criteria
+        names = super().name_search(
+            name=name, args=args, operator=operator, limit=limit
+        )
+        # Add more details to quickly identify a ticket
+        result = []
+        for item in names:
+            rec = self.browse(item[0])[0]
+            name = item[1]
+            if rec.team_id:
+                name = ('%s (%s)') % (name, rec.team_id.name)
+            if rec.stage_id and not rec.stage_id.name[0].isalpha():
+                emoji = rec.stage_id.name[0]
+                name = ('%s %s') % (emoji, name)
+            result.append((item[0], name))
+        return result
+
+
+    @api.model
+    def _name_search(
+        self, name, args=None, operator='ilike', limit=100, name_get_uid=None
+    ):
+        if not args:
+            args = []
+        if name:
+            positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            record_ids = []
+            if operator in positive_operators:
+                record_ids = self._search(
+                    [('number', '=', name)] + args,
+                    limit=limit,
+                    access_rights_uid=name_get_uid
+                )
+            if not record_ids and operator not in expression.NEGATIVE_TERM_OPERATORS:
+                # Do not merge the 2 next lines into one single search, SQL
+                # search performance would be abysmal on a database with
+                # thousands of matching products, due to the huge merge+unique
+                # needed for the OR operator (and given the fact that the
+                # 'name' lookup results come from the ir.translation table
+                # Performing a quick memory merge of ids in Python will give
+                # much better performance
+                record_ids = self._search(
+                    args + [('number', operator, name)], limit=limit
+                )
+                if not limit or len(record_ids) < limit:
+                    # we may underrun the limit because of dupes in the
+                    # results, that's fine
+                    limit2 = (limit - len(record_ids)) if limit else False
+                    product2_ids = self._search(
+                        args + [
+                            ('name', operator, name),
+                            ('id', 'not in', record_ids)
+                        ],
+                        limit=limit2,
+                        access_rights_uid=name_get_uid
+                    )
+                    record_ids.extend(product2_ids)
+            elif not record_ids and operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain = expression.OR(
+                    [
+                        [
+                            '&',
+                            ('number', operator, name),
+                            ('name', operator, name),
+                        ],
+                        [
+                            '&',
+                            ('number', '=', False),
+                            ('name', operator, name),
+                        ],
+                    ]
+                )
+                domain = expression.AND([args, domain])
+                record_ids = self._search(
+                    domain, limit=limit, access_rights_uid=name_get_uid
+                )
+            if not record_ids and operator in positive_operators:
+                ptrn = re.compile('(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    record_ids = self._search(
+                        [('number', '=', res.group(2))] + args,
+                        limit=limit,
+                        access_rights_uid=name_get_uid
+                    )
+        else:
+            record_ids = self._search(
+                args, limit=limit, access_rights_uid=name_get_uid
+            )
+        return self.browse(record_ids).name_get()

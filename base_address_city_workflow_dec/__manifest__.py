067756b4acb9b828033d34c86f1ca7f748de1251
Yann Papouin <y.papouin@dec-industrie.com>
2021-05-21 11:16:59 +0200

    New module to search cities directly using zip codes

---
+++ base_address_city_workflow_dec/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Base Address City Workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'depends': [
+        'base_address_city',
+        'base_location',
+    ],
+    'data': [
+        'views/res_city.xml',
+    ],
+    'installable': True
+}

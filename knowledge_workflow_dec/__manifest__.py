8c31c2d4078e8f72d11fb5a90befdb3a2f8a151d
Yann Papouin <y.papouin@dec-industrie.com>
2021-06-01 22:35:59 +0200

    Workflow for knowlege modules

---
+++ knowledge_workflow_dec/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Knowledge Workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.dec-industrie.com',
+    'summary': '''Custom views for knowledge''',
+    'depends': [
+        'document_page',
+        'document_page_reference',
+    ],
+    'data': [
+        'views/document_page.xml',
+    ],
+    'installable': True
+}

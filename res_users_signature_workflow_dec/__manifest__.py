f204ccb43905dc7e7d98628fbfcb3d650f6b1af1
Yann Papouin <ypa@decgroupe.com>
2021-07-08 13:03:14 +0200

    DEC signatures

---
+++ res_users_signature_workflow_dec/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Users signature (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Base',
+    'summary': "DEC signatures",
+    'depends': [
+        'res_users_signature',
+    ],
+    'data':
+        [
+            'data/signature_template.xml',
+        ],
+    'installable': True
+}

f0996820aab475521e99dd09d3f254a4bc4b58a3
Yann Papouin <ypa@decgroupe.com>
2021-10-22 14:29:34 +0200

    Indicate when a form field is required with a color

---
+++ web_workflow_dec/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Web workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': 'Web',
+    'depends': [
+        'web',
+        'dec',
+    ],
+    'data': ['views/assets.xml', ],
+    'installable': True
+}

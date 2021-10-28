6b21a8a41abf618c42372804f28b078b940b5c32
Yann Papouin <ypa@decgroupe.com>
2021-10-28 14:23:54 +0200

    Default tasks and projects

--- project_workflow_dec/__manifest__.py
+++ project_workflow_dec/__manifest__.py
@@ -3,20 +3,27 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Custom views for project''',
-    'depends': [
-        'dec',
-        'hr_timesheet',
-        'sale_timesheet',
-        'sale_timesheet_line_exclude',
-        'project_category',
-        'project_identification',
-        'web_kanban_draggable',
-    ],
-    'data': [
-        'views/assets.xml',
-        'views/project_project.xml',
-        'views/project_task.xml',
-    ],
+    'summary': "Custom views for project",
+    'depends':
+        [
+            'dec',
+            'hr_timesheet',
+            'sale_timesheet',
+            'sale_timesheet_line_exclude',
+            'sale_timesheet_task_exclude',
+            'project_category',
+            'project_identification',
+            'web_kanban_draggable',
+        ],
+    'data':
+        [
+            'views/assets.xml',
+            'views/project_project.xml',
+            'views/project_task.xml',
+            'data/project_type.xml',
+            'data/project.xml',
+            'data/project_task_type.xml',
+            'data/project_task.xml',
+        ],
     'installable': True
 }

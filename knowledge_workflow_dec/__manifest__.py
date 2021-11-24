88974e6e96b5a749a87838e2966dea6557fbcdc4
Yann Papouin <ypa@decgroupe.com>
2021-11-24 09:15:00 +0100

    Reset font style to normal for document pages

--- knowledge_workflow_dec/__manifest__.py
+++ knowledge_workflow_dec/__manifest__.py
@@ -3,12 +3,13 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Custom views for knowledge''',
+    'summary': "Custom views for knowledge",
     'depends': [
         'document_page',
         'document_page_reference',
     ],
     'data': [
+        'views/assets.xml',
         'views/document_page.xml',
     ],
     'installable': True

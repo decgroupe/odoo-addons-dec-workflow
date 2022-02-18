646874666a9fc09b5ceae9cd5c12be9a1db21c1a
Yann Papouin <ypa@decgroupe.com>
2022-02-18 18:01:50 +0100

    Dedicated customization module for module search filter

---
+++ base_module_workflow_dec/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Module workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Add built-in search for modules with autors credited to DEC",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ir_module.xml',
+    ],
+    'installable': True
+}

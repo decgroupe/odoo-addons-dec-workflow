d2d9ae768a4db0baec12c9aa3edb4fad1ba0871d
Yann Papouin <ypa@decgroupe.com>
2022-09-05 16:49:15 +0200

    Remove useless `action_view_sales` and `action_view_po` stat buttons

---
+++ product_form_quotation_link_workflow_dec/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Form Quotation workflow (DEC)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'depends': ['product_form_quotation_link', ],
+    'data': [
+        'views/product_template.xml',
+        'views/product_product.xml',
+    ],
+    'installable': True
+}

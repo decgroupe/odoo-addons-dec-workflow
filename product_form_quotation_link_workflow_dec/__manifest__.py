2d61fee5bb88d064e1f82ce9ae5ed5ff17004bf7
Yann Papouin <ypa@decgroupe.com>
2022-09-08 09:56:54 +0200

    [FIX] Element not found in parent view
    Somes views need a `groups_id` and a new customization module is needed for `product_form_sale_link`

--- product_form_quotation_link_workflow_dec/__manifest__.py
+++ product_form_quotation_link_workflow_dec/__manifest__.py
@@ -3,7 +3,15 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'depends': ['product_form_quotation_link', ],
+    'depends':
+        [
+            'product_form_sale_link',
+            'product_form_purchase_link',
+            # Note that these modules (`product_form_sale_link` and
+            # `product_form_purchase_link`) already are a dependency for
+            # `product_form_quotation_link`
+            'product_form_quotation_link',
+        ],
     'data': [
         'views/product_template.xml',
         'views/product_product.xml',

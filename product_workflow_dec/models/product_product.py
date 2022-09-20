99fb1eb17b7a0daaa84aaeb67c48e74dbddf4821
Yann Papouin <ypa@decgroupe.com>
2022-09-20 11:38:16 +0200

    [IMP] product_workflow_dec: Override `purchased_product_qty` to remove 365 days filtering

--- product_workflow_dec/models/product_product.py
+++ product_workflow_dec/models/product_product.py
@@ -3,10 +3,34 @@
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
 from odoo import api, fields, models
+from odoo.tools.float_utils import float_round
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
+
+    @api.multi
+    def _compute_purchased_product_qty(self):
+        # Override default implementation to remove the 365 days domain filter
+        # from ./odoo/addons/purchase/models/product.py
+        domain = [
+            ('state', 'in', ['purchase', 'done']),
+            ('product_id', 'in', self.mapped('id')),
+        ]
+        order_lines = self.env['purchase.order.line'].read_group(
+            domain, ['product_id', 'product_uom_qty'], ['product_id']
+        )
+        purchased_data = dict(
+            [
+                (data['product_id'][0], data['product_uom_qty'])
+                for data in order_lines
+            ]
+        )
+        for product in self:
+            product.purchased_product_qty = float_round(
+                purchased_data.get(product.id, 0),
+                precision_rounding=product.uom_id.rounding
+            )
 
     @api.model
     def get_default_market_bom_products(self):

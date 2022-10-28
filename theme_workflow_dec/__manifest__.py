e080e7dc3241d3bd752d7b87f2896501b6f2f2be
Yann Papouin <ypa@decgroupe.com>
2022-10-28 12:22:02 +0200

    [ADD] theme_workflow_dec: Initial bootswatch theme override for DEC

---
+++ theme_workflow_dec/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Bootswatch Theme workflow (DEC)',
+    'category': 'Theme/Corporate',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': 'Website',
+    'depends': [
+        'website',
+        'website_theme_install',
+        'theme_bootswatch',
+    ],
+    'data': [
+        'views/theme_bootswatch_templates.xml',
+    ],
+    'installable': True
+}

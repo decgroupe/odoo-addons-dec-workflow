01fa6e2bb9a17041f3dc34ea8bab472698ecf499
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:53 +0100

    [MIG] theme_workflow_dec: Migration to 14.0

--- theme_workflow_dec/__manifest__.py
+++ theme_workflow_dec/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Bootswatch Theme workflow (DEC)',
     'category': 'Theme/Corporate',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': 'Website',

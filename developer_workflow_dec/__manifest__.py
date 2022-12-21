2c6bbf4300b031cd3758693f951d1444a526be14
Yann Papouin <ypa@decgroupe.com>
2022-12-21 17:57:07 +0100

    [ADD] developer_workflow_dec: New module based on `developer_menu` from Akretion

---
+++ developer_workflow_dec/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Developer (DEC)',
+    'version': "14.0.1.0.0",
+    'author': 'DEC, Yann Papouin, Akretion',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Menu Shortcut for developer usage",
+    'depends': [
+        'base',
+        'mail',
+    ],
+    'data': ['views/menu.xml', ],
+    'installable': True
+}

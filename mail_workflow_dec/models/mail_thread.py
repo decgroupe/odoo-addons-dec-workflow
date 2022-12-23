5e1bc0a0bc8e7243e66c6de84171162bd7b60a0e
Yann Papouin <ypa@decgroupe.com>
2022-12-23 10:15:08 +0100

    [MIG] calendar_workflow_dec: Use new `message_notify` signature

--- mail_workflow_dec/models/mail_thread.py
+++ mail_workflow_dec/models/mail_thread.py
@@ -7,11 +7,31 @@ from odoo import _, api, models
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def message_notify(self, partner_ids, body='', subject=False, **kwargs):
+    def message_notify(
+        self,
+        *,
+        partner_ids=False,
+        parent_id=False,
+        model=False,
+        res_id=False,
+        author_id=None,
+        email_from=None,
+        body='',
+        subject=False,
+        **kwargs
+    ):
         if body:
             if isinstance(body, bytes):
                 body = body.decode('utf-8')
             body = self.env['mail.template']._hard_replace(body)
         super().message_notify(
-            partner_ids, body=body, subject=subject, **kwargs
+            partner_ids=partner_ids,
+            parent_id=parent_id,
+            model=model,
+            res_id=res_id,
+            author_id=author_id,
+            email_from=email_from,
+            body=body,
+            subject=subject,
+            **kwargs
         )

bdb7444130b86e0eaa5fd3af0c012d1798536ece
Yann Papouin <ypa@decgroupe.com>
2023-05-25 13:22:05 +0200

    [IMP] sale_workflow_dec: black, isort, prettier, readme, icon

--- sale_crm_workflow_dec/models/crm_lead.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) DEC SARL, Inc - All Rights Reserved.
-# Written by Yann Papouin <ypa at decgroupe.com>, Dec 2020
-
-from odoo import fields, models
-
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'

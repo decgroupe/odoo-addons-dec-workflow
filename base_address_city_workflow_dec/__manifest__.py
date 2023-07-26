21133fc00bc2892f1fea8acb682e5f819c46a652
Yann Papouin <ypa@decgroupe.com>
2023-07-26 14:40:24 +0200

    [IMP] base_address_city_workflow_dec: black, isort, prettier, readme, icon

--- base_address_city_workflow_dec/__manifest__.py
+++ base_address_city_workflow_dec/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': 'Base Address City Workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'depends': [
-        'base_address_city',
-        'base_location',
+    "name": "Base Address City Workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base_address_city",
+        "base_location",
     ],
-    'data': [
-        'views/res_city.xml',
+    "data": [
+        "views/res_city.xml",
     ],
-    'installable': True
+    "installable": True,
 }

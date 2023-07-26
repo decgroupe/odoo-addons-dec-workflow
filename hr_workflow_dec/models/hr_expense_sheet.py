28f4e4d69b901b0639d69d0f8b1f741ba9eb7049
Yann Papouin <ypa@decgroupe.com>
2023-07-26 15:25:23 +0200

    [IMP] hr_workflow_dec: black, isort, prettier, readme, icon

--- hr_workflow_dec/models/hr_expense_sheet.py
+++ hr_workflow_dec/models/hr_expense_sheet.py
@@ -1,7 +1,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Dec 2021
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class HrExpenseSheet(models.Model):
@@ -11,19 +11,18 @@ class HrExpenseSheet(models.Model):
     def create(self, vals):
         # Disable subscribe notify for expense sheet on create
         rec = super(
-            HrExpenseSheet,
-            self.with_context(mail_auto_subscribe_no_notify=True)
+            HrExpenseSheet, self.with_context(mail_auto_subscribe_no_notify=True)
         ).create(vals)
         return rec
 
-    @api.onchange('employee_id')
+    @api.onchange("employee_id")
     def _onchange_employee_id(self):
         super()._onchange_employee_id()
-        ICP = self.env['ir.config_parameter'].sudo()
-        default_user_id = ICP.get_param('hr_workflow_dec.default_user_id')
+        ICP = self.env["ir.config_parameter"].sudo()
+        default_user_id = ICP.get_param("hr_workflow_dec.default_user_id")
         if default_user_id:
-            default_user_id = self.env['res.users'].search(
-                [('id', '=', default_user_id)], limit=1
+            default_user_id = self.env["res.users"].search(
+                [("id", "=", default_user_id)], limit=1
             )
             if default_user_id:
                 self.user_id = default_user_id

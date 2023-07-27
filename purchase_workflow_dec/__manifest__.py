d42cc99f5ba9ec9e0baf2220e9cb42e3a8dd8974
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:19:47 +0200

    [IMP] purchase_workflow_dec: black, isort, prettier, readme, icon

--- purchase_workflow_dec/__manifest__.py
+++ purchase_workflow_dec/__manifest__.py
@@ -1,21 +1,19 @@
 {
-    'name': 'Purchase workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Purchase order",
-    'depends':
-        [
-            'purchase',
-            'purchase_order_line_stock_available',
-            'web',
-        ],
-    'data':
-        [
-            'views/assets.xml',
-            'views/purchase_order.xml',
-            'views/purchase_order_line.xml',
-            'templates/mail.xml',
-        ],
-    'installable': True
+    "name": "Purchase workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "summary": "Purchase order",
+    "depends": [
+        "purchase",
+        "purchase_order_line_stock_available",
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/purchase_order.xml",
+        "views/purchase_order_line.xml",
+        "templates/mail.xml",
+    ],
+    "installable": True,
 }
--- purchase_workflow_dec/models/purchase_order.py
+++ purchase_workflow_dec/models/purchase_order.py
@@ -1,17 +1,18 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2020
 
-from odoo import models, fields, api
+from odoo import models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
     _order = "id desc"
 
     def action_rfq_send(self):
         view = super().action_rfq_send()
         # Do not set layout to "mail.mail_notification_paynow" since we
         # don't want that our supplier access our portal
-        view['context']['custom_layout'] = \
-            'purchase_workflow_dec.mail_notification_paynow_nobutton'
+        view["context"][
+            "custom_layout"
+        ] = "purchase_workflow_dec.mail_notification_paynow_nobutton"
         return view

594345cf15bd50073697ff686bb8a7e499a74abd
Yann Papouin <ypa@decgroupe.com>
2023-07-27 09:23:41 +0200

    [IMP] knowledge_workflow_dec: black, isort, prettier, readme, icon

--- knowledge_workflow_dec/__manifest__.py
+++ knowledge_workflow_dec/__manifest__.py
@@ -1,16 +1,15 @@
 {
-    'name': 'Knowledge Workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Custom views for knowledge",
-    'depends': [
-        'document_page',
-        'document_page_reference',
+    "name": "Knowledge Workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "document_page",
+        "document_page_reference",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/document_page.xml',
+    "data": [
+        "views/assets.xml",
+        "views/document_page.xml",
     ],
-    'installable': True
+    "installable": True,
 }

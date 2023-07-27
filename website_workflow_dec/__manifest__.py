6241708bdc0b87ebad27d21d193bd9bed7daccb0
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:34:37 +0200

    [IMP] website_workflow_dec: black, isort, prettier, readme, icon

--- website_workflow_dec/__manifest__.py
+++ website_workflow_dec/__manifest__.py
@@ -1,10 +1,13 @@
 {
-    'name': 'Website workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': 'Website',
-    'depends': ['website', ],
-    'data': ['security/model_security.xml'],
-    'installable': True
+    "name": "Website workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "security/model_security.xml",
+    ],
+    "installable": True,
 }

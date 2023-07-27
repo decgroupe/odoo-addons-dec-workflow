34134087deeb3a029da766632ecee62d3acc2760
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:21:41 +0200

    [IMP] res_users_signature_workflow_dec: black, isort, prettier, readme, icon

--- res_users_signature_workflow_dec/__manifest__.py
+++ res_users_signature_workflow_dec/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    'name': 'Users signature (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'category': 'Base',
-    'summary': "DEC signatures",
-    'depends': [
-        'res_users_signature',
+    "name": "Users signature (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "category": "Base",
+    "depends": [
+        "res_users_signature",
     ],
-    'data':
-        [
-            'data/signature_template.xml',
-        ],
-    'installable': True
+    "data": [
+        "data/signature_template.xml",
+    ],
+    "installable": True,
 }

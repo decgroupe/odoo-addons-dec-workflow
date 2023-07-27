c037893cb31a59ebde559655c61342efbe47b624
Yann Papouin <ypa@decgroupe.com>
2023-07-27 14:00:21 +0200

    [IMP] mail_tracking_workflow: black, isort, prettier, readme, icon

--- mail_tracking_workflow/__manifest__.py
+++ mail_tracking_workflow/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    'name': 'Email tracking workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'category': 'Sales',
-    'summary': '''Custom rendering''',
-    'depends': [
-        'mail_tracking',
+    "name": "Email tracking workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "category": "Sales",
+    "depends": [
+        "mail_tracking",
     ],
-    'data':
-        [
-            'views/assets.xml',
-        ],
-    'installable': True
+    "data": [
+        "views/assets.xml",
+    ],
+    "installable": True,
 }

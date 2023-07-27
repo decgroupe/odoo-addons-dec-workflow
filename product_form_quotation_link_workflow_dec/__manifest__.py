31be0db7dd011b4a5b57096dfbeeeee761120753
Yann Papouin <ypa@decgroupe.com>
2023-07-27 14:57:16 +0200

    [IMP] product_form_quotation_link_workflow_dec: black, isort, prettier, readme, icon

--- product_form_quotation_link_workflow_dec/__manifest__.py
+++ product_form_quotation_link_workflow_dec/__manifest__.py
@@ -1,20 +1,19 @@
 {
-    'name': 'Product Form Quotation workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'depends':
-        [
-            'product_form_sale_link',
-            'product_form_purchase_link',
-            # Note that these modules (`product_form_sale_link` and
-            # `product_form_purchase_link`) already are a dependency for
-            # `product_form_quotation_link`
-            'product_form_quotation_link',
-        ],
-    'data': [
-        'views/product_template.xml',
-        'views/product_product.xml',
+    "name": "Product Form Quotation workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "product_form_sale_link",
+        "product_form_purchase_link",
+        # Note that these modules (`product_form_sale_link` and
+        # `product_form_purchase_link`) already are a dependency for
+        # `product_form_quotation_link`
+        "product_form_quotation_link",
     ],
-    'installable': True
+    "data": [
+        "views/product_template.xml",
+        "views/product_product.xml",
+    ],
+    "installable": True,
 }

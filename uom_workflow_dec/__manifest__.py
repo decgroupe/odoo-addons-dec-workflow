2e654e68f40cdeba3d97460aa2a5bbcf1a91afd4
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:29:56 +0200

    [IMP] uom_workflow_dec: black, isort, prettier, readme, icon

--- uom_workflow_dec/__manifest__.py
+++ uom_workflow_dec/__manifest__.py
@@ -1,11 +1,14 @@
 {
-    'name': 'UoM Workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Custom hour/day ratio",
-    'depends': ['uom', ],
-    "data": ['data/uom.xml', ],
-    'installable': True,
-    'post_init_hook': 'post_init',
+    "name": "UoM Workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "uom",
+    ],
+    "data": [
+        "data/uom.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init",
 }

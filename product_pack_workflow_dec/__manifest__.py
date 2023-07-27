2beff5c25cae6b8c9dd7f7036cba5b4ff9371a73
Yann Papouin <ypa@decgroupe.com>
2023-07-27 14:59:28 +0200

    [IMP] product_pack_workflow_dec: black, isort, prettier, readme, icon

--- product_pack_workflow_dec/__manifest__.py
+++ product_pack_workflow_dec/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    'name': 'Product Pack workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': 'Format',
-    'depends': ['sale_product_pack', ],
-    'data': [],
-    'installable': True
+    "name": "Product Pack workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "summary": "Format",
+    "depends": [
+        "sale_product_pack",
+    ],
+    "data": [],
+    "installable": True,
 }
--- product_pack_workflow_dec/models/product_pack_line.py
+++ product_pack_workflow_dec/models/product_pack_line.py
@@ -1,14 +1,13 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jun 2021
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class ProductPack(models.Model):
-    _inherit = 'product.pack.line'
+    _inherit = "product.pack.line"
 
-    
     def get_sale_order_line_vals(self, line, order):
         vals = super().get_sale_order_line_vals(line, order)
-        vals['name'] = vals['name'].replace('> ', '🢖 ')
+        vals["name"] = vals["name"].replace("> ", "🢖 ")
         return vals

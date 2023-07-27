5aa24ed15719fed85f18c343bfa1351175b3367b
Yann Papouin <ypa@decgroupe.com>
2023-07-27 14:55:08 +0200

    [IMP] mrp_production_request_workflow_dec: black, isort, prettier, readme, icon

--- mrp_production_request_workflow_dec/__manifest__.py
+++ mrp_production_request_workflow_dec/__manifest__.py
@@ -1,15 +1,15 @@
 {
-    'name': 'MRP Production Request Workflow',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Group customization''',
-    'depends': ['mrp_production_request', ],
-    'data':
-        [
-            "security/model_security.xml",
-            "security/ir.model.access.csv",
-            "views/mrp_production_request.xml",
-        ],
-    'installable': True
+    "name": "MRP Production Request Workflow",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mrp_production_request",
+    ],
+    "data": [
+        "security/model_security.xml",
+        "security/ir.model.access.csv",
+        "views/mrp_production_request.xml",
+    ],
+    "installable": True,
 }

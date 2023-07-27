53547b3be82f2863144e0afbbda634bf7322f99e
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:24:34 +0200

    [IMP] sale_timesheet_workflow_dec: black, isort, prettier, readme, icon

--- sale_timesheet_workflow_dec/__manifest__.py
+++ sale_timesheet_workflow_dec/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    'name': 'Sale Timesheet workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'category': 'Sales',
-    'summary': "Custom views for sale order",
-    'depends': ['sale_timesheet', ],
-    'data': [],
-    'installable': True
+    "name": "Sale Timesheet workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "category": "Sales",
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [],
+    "installable": True,
 }

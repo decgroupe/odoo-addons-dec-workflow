a59b2ed3d57efa4f3e3922608a14c4c51f21323a
Yann Papouin <ypa@decgroupe.com>
2023-07-27 14:52:02 +0200

    [IMP] partner_workflow_dec: black, isort, prettier, readme, icon

--- partner_workflow_dec/__manifest__.py
+++ partner_workflow_dec/__manifest__.py
@@ -1,15 +1,14 @@
 {
-    'name': 'Partner workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': 'Add extra fields on default views',
-    'depends': [
-        'base',
-        'base_location',
+    "name": "Partner workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base",
+        "base_location",
     ],
-    'data': [
-        'views/partner.xml',
+    "data": [
+        "views/partner.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- partner_workflow_dec/models/res_partner.py
+++ partner_workflow_dec/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Nov 2020
 
-from odoo import _, api, models, fields
+from odoo import api, models
 
 
 class ResPartner(models.Model):
@@ -17,19 +17,17 @@ class ResPartner(models.Model):
         # When linking a contact to a partner, change the adress type to
         # `other`, otherwise existing address will be overriden with the
         # the parent one
-        if 'parent_id' in vals and not 'type' in vals:
-            vals['type'] = 'other'
-        if vals.get('type') == 'private':
-            vals['vat'] = False
+        if "parent_id" in vals and not "type" in vals:
+            vals["type"] = "other"
+        if vals.get("type") == "private":
+            vals["vat"] = False
         res = super().write(vals)
         self._clear_vat()
         return res
 
     def _clear_vat(self):
-        if not self.env.context.get('clearing_vat'):
-            private_partners = self.filtered('vat').filtered(
-                lambda rec: rec.type == 'private'
+        if not self.env.context.get("clearing_vat"):
+            private_partners = self.filtered("vat").filtered(
+                lambda rec: rec.type == "private"
             )
-            private_partners.with_context(clearing_vat=True).write(
-                {'vat': False}
-            )
+            private_partners.with_context(clearing_vat=True).write({"vat": False})

e94890e706dab2a7299bc627a833faf095fb131c
Yann Papouin <ypa@decgroupe.com>
2023-07-27 09:34:44 +0200

    [IMP] mail_activity_workflow_dec: black, isort, prettier, readme, icon

--- mail_activity_workflow_dec/__manifest__.py
+++ mail_activity_workflow_dec/__manifest__.py
@@ -1,16 +1,15 @@
 {
-    'name': 'Mail activity workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''View customization''',
-    'depends': [
-        'mail',
-        'mail_activity_board',
-        'mail_activity_team',
+    "name": "Mail activity workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mail",
+        "mail_activity_board",
+        "mail_activity_team",
     ],
-    'data': [
-        'views/mail_activity.xml',
+    "data": [
+        "views/mail_activity.xml",
     ],
-    'installable': True
+    "installable": True,
 }

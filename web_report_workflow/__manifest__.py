bd2871f3e805e791126a5a0d004d70192573defd
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:30:50 +0200

    [IMP] web_report_workflow: black, isort, prettier, readme, icon

--- web_report_workflow/__manifest__.py
+++ web_report_workflow/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    'name': 'Web Report Workflow',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Report template improvements''',
-    'depends': [
-        'web',
-        'company_report',
+    "name": "Web Report Workflow",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "web",
+        "company_report",
     ],
-    'data':
-        [
-            'views/report_templates.xml',
-        ],
-    'installable': True
+    "data": [
+        "views/report_templates.xml",
+    ],
+    "installable": True,
 }

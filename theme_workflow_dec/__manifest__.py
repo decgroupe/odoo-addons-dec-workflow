ea6b310148de71deae119546661e31ad25f97308
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:29:03 +0200

    [IMP] theme_workflow_dec: black, isort, prettier, readme, icon

--- theme_workflow_dec/__manifest__.py
+++ theme_workflow_dec/__manifest__.py
@@ -1,17 +1,16 @@
 {
-    'name': 'Bootswatch Theme workflow (DEC)',
-    'category': 'Theme/Corporate',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': 'Website',
-    'depends': [
-        'website',
-        'website_theme_install',
-        'theme_bootswatch',
+    "name": "Bootswatch Theme workflow (DEC)",
+    "category": "Theme/Corporate",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "website",
+        "website_theme_install",
+        "theme_bootswatch",
     ],
-    'data': [
-        'views/theme_bootswatch_templates.xml',
+    "data": [
+        "views/theme_bootswatch_templates.xml",
     ],
-    'installable': True
+    "installable": True,
 }

9469ad2a9489ff176bba54053c22462fc3261611
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:31:58 +0200

    [IMP] web_workflow_dec: black, isort, prettier, readme, icon

--- web_workflow_dec/__manifest__.py
+++ web_workflow_dec/__manifest__.py
@@ -1,16 +1,15 @@
 {
-    'name': 'Web workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': 'Web',
-    'depends': [
-        'auth_signup',
-        'auth_unique_link',
+    "name": "Web workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "auth_signup",
+        "auth_unique_link",
     ],
-    'data': [
-        'views/assets.xml',
-        'templates/login_templates.xml',
+    "data": [
+        "views/assets.xml",
+        "templates/login_templates.xml",
     ],
-    'installable': True
+    "installable": True,
 }

1a33cef87d7393f10179ae3d5b0eb6661cb7d10a
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:20:45 +0200

    [IMP] res_partner_workflow_dec: black, isort, prettier, readme, icon

--- res_partner_workflow_dec/__manifest__.py
+++ res_partner_workflow_dec/__manifest__.py
@@ -1,10 +1,13 @@
 {
-    'name': 'Partner Workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Custom views for partner",
-    'depends': ['base', ],
-    'data': ['views/res_partner.xml', ],
-    'installable': True
+    "name": "Partner Workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "installable": True,
 }

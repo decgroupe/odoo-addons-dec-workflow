a4b660c81fee27281d7946e51ff2d6f86c91284c
Yann Papouin <ypa@decgroupe.com>
2023-07-27 15:27:35 +0200

    [IMP] stock_workflow_dec: black, isort, prettier, readme, icon

--- stock_workflow_dec/__manifest__.py
+++ stock_workflow_dec/__manifest__.py
@@ -1,22 +1,20 @@
 {
-    'name': 'Stock workflow (DEC)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Stock and pickings",
-    'depends': [
-        'stock',
-        'stock_split_procure_method',
-        'stock_mts_mto_rule',
+    "name": "Stock workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "stock",
+        "stock_split_procure_method",
+        "stock_mts_mto_rule",
     ],
-    'data':
-        [
-            'data/picking_type.xml',
-            'data/stock_location.xml',
-            'data/stock_warehouse.xml',
-            'views/assets.xml',
-            'views/stock_move.xml',
-            'views/stock_picking.xml',
-        ],
-    'installable': True
+    "data": [
+        "data/picking_type.xml",
+        "data/stock_location.xml",
+        "data/stock_warehouse.xml",
+        "views/assets.xml",
+        "views/stock_move.xml",
+        "views/stock_picking.xml",
+    ],
+    "installable": True,
 }

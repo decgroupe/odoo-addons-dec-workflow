3dca6ae1d66809c314495819e347b698023e1bbc
Yann Papouin <ypa@decgroupe.com>
2023-10-27 16:14:54 +0200

    [FIX] hr_workflow_dec: Error raised when creating a new sheet

--- hr_workflow_dec/models/hr_expense_sheet.py
+++ hr_workflow_dec/models/hr_expense_sheet.py
@@ -15,9 +15,9 @@ class HrExpenseSheet(models.Model):
         ).create(vals)
         return rec
 
-    @api.onchange("employee_id")
-    def _onchange_employee_id(self):
-        super()._onchange_employee_id()
+    @api.depends('employee_id')
+    def _compute_from_employee_id(self):
+        super()._compute_from_employee_id()
         ICP = self.env["ir.config_parameter"].sudo()
         default_user_id = ICP.get_param("hr_workflow_dec.default_user_id")
         if default_user_id:
@@ -25,4 +25,7 @@ class HrExpenseSheet(models.Model):
                 [("id", "=", default_user_id)], limit=1
             )
             if default_user_id:
-                self.user_id = default_user_id
+                for sheet in self:
+                    # only override manager if no one is set for this employee
+                    if not sheet.user_id or not sheet.employee_id.expense_manager_id:
+                        sheet.user_id = default_user_id

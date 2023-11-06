d7fea16917353ef7b58a5816e154dec0482e6d6a
Yann Papouin <ypa@decgroupe.com>
2023-11-06 10:12:41 +0100

    [IMP] sale_workflow_dec: Explicit amount labels (including/excluding taxes)

--- sale_workflow_dec/models/sale_order.py
+++ sale_workflow_dec/models/sale_order.py
@@ -1,11 +1,21 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Mar 2020
 
-from odoo import models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    amount_untaxed = fields.Monetary(
+        string="Total Amount (excluding taxes)",
+    )
+    amount_total = fields.Monetary(
+        string="Total Amount (including taxes)",
+    )
+    margin = fields.Monetary(
+        "Margin (excluding taxes)",
+    )
 
     def copy(self, default=None):
         if self.origin:

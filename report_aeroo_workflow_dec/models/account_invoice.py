3eba608b385fbd38d0b8aafd4fb7199c1cf5933b
Yann Papouin <ypa@decgroupe.com>
2024-02-16 11:00:30 +0100

    [FIX] report_aeroo_workflow_dec: Our report was not the default one anymore

--- report_aeroo_workflow_dec/models/account_invoice.py
+++ report_aeroo_workflow_dec/models/account_invoice.py
@@ -9,11 +9,11 @@ class AccountMove(models.Model):
 
     # Override default report with aeroo one
 
-    def invoice_print(self):
+    def action_invoice_print(self):
         """Print the invoice and mark it as sent, so that we can see more
         easily the next step of the workflow
         """
-        super().invoice_print()
+        super().action_invoice_print()
         if self.user_has_groups("account.group_account_invoice"):
             return (
                 self.env.ref("account_report_aeroo.account_invoice_report")

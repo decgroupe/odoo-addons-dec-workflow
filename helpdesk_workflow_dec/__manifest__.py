4a5f35e2a2d144570bedf770a7951f96f4b7958b
Yann Papouin <ypa@decgroupe.com>
2024-03-26 12:00:55 +0100

    [IMP] helpdesk_workflow_dec: Template redesign (created ticket)

--- helpdesk_workflow_dec/__manifest__.py
+++ helpdesk_workflow_dec/__manifest__.py
@@ -6,9 +6,12 @@
     "depends": [
         "project_identification",
         "helpdesk_mgmt_timesheet",
+        "mail_qweb",
     ],
     "data": [
         "data/project.xml",
+        "data/ir_ui_view.xml",
+        "data/mail_template.xml",
         "views/helpdesk_ticket.xml",
     ],
     "installable": True,

040a74e950fa3ba41f9d68ca5b6135156e3d065b
Yann Papouin <ypa@decgroupe.com>
2024-04-12 14:49:48 +0200

    [ADD] software_workflow_dec: Use new QWeb way to re-design templates

---
+++ software_workflow_dec/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Software workflow (DEC)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "software_license_pass",
+        "auth_signup_delegate_fencing",  # for pass email template
+    ],
+    "data": [
+        "data/ir_ui_view.xml",
+        "data/mail_template.xml",
+    ],
+    "installable": True,
+}

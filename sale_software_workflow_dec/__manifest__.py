fc466096ea8a920705d4134e85d00286847450f0
Yann Papouin <ypa@decgroupe.com>
2024-04-12 14:11:50 +0200

    [FIX] sale_software_workflow_dec: Module renamed from software_workflow_dec

--- sale_software_workflow_dec/__manifest__.py
+++ sale_software_workflow_dec/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Software workflow (DEC)",
+    "name": "Sale Software workflow (DEC)",
     "version": "14.0.1.0.0",
     "author": "DEC",
     "website": "https://www.decgroupe.com",

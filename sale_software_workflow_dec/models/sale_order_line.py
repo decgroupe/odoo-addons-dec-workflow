383f8b42d3980d29f9d567af5943c5fef1604326
Yann Papouin <ypa@decgroupe.com>
2024-11-15 17:10:57 +0100

    [FIX] sale_software_workflow_dec: A pass must exists before creating an activity

--- sale_software_workflow_dec/models/sale_order_line.py
+++ sale_software_workflow_dec/models/sale_order_line.py
@@ -10,16 +10,17 @@ class SaleOrderLine(models.Model):
 
     def _create_application_pass(self):
         pass_id = super()._create_application_pass()
-        digital_team_id = self.env.ref(
-            "mail_activity_workflow_dec.team_design_office_digital"
-        )
-        if digital_team_id:
-            pass_id.with_context(
-                mail_activity_noautofollow=True,
-            ).activity_schedule(
-                act_type_xmlid="mail.mail_activity_data_todo",
-                note=_("🚨 Auto: To Send"),
-                user_id=digital_team_id.user_id.id,
-                team_id=digital_team_id.id,
+        if pass_id:
+            digital_team_id = self.env.ref(
+                "mail_activity_workflow_dec.team_design_office_digital"
             )
+            if digital_team_id:
+                pass_id.with_context(
+                    mail_activity_noautofollow=True,
+                ).activity_schedule(
+                    act_type_xmlid="mail.mail_activity_data_todo",
+                    note=_("🚨 Auto: To Send"),
+                    user_id=digital_team_id.user_id.id,
+                    team_id=digital_team_id.id,
+                )
         return pass_id

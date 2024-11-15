35c2017ec74f7c4062a9ca76b550be7e2697839f
Yann Papouin <ypa@decgroupe.com>
2024-11-15 17:08:13 +0100

    [FIX] purchase_workflow_dec: Use current date to set purchase order name

--- purchase_workflow_dec/models/purchase_order.py
+++ purchase_workflow_dec/models/purchase_order.py
@@ -1,12 +1,31 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2020
 
-from odoo import models
+from odoo import api, models, fields
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
     _order = "id desc"
+
+    @api.model
+    def create(self, vals):
+        # set name from sequence before built-in purchase.create to use the current
+        # date as reference
+        company_id = vals.get(
+            "company_id", self.default_get(["company_id"])["company_id"]
+        )
+        self_comp = self.with_company(company_id)
+        if vals.get("name", "New") == "New":
+            seq_date = fields.Datetime.now()
+            vals["name"] = (
+                self_comp.env["ir.sequence"].next_by_code(
+                    "purchase.order", sequence_date=seq_date
+                )
+                or "/"
+            )
+        res = super(PurchaseOrder, self_comp).create(vals)
+        return res
 
     def action_rfq_send(self):
         view = super().action_rfq_send()
